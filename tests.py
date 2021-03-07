4fa7b5508a1d18987613a8df6a1280437a352aba
Piotr Podgorski <peter.podgorski@gmail.com>
2021-03-07 12:50:24 +0100

    Implemented and.

--- tests.py
+++ tests.py
@@ -16,6 +16,8 @@ F = KI  # False
 NOT = lambda b: b(F)(T)  # where b is T or F
 C_NOT = lambda b: C(b)
 
+A = lambda p: lambda q: p(q)(p)  #
+
 
 def test_identity_or_1_is_1():
     assert I(1) == 1
@@ -59,3 +61,17 @@ class Test_negation_selects_the_other_boolean:
 class Test_Cardinal_impl_of_NOT:
     def test_C_NOT_of_T_of_1_2_behaves_like_F_of_1_2(self):
         assert C_NOT(T)(1)(2) == F(1)(2)
+
+
+class Test_And:
+    def test_of_T_T_is_T(self):
+        assert A(T)(T) == T
+
+    def test_of_T_F_is_F(self):
+        assert A(T)(F) == F
+
+    def test_of_F_T_is_F(self):
+        assert A(F)(T) == F
+
+    def test_of_F_F_is_F(self):
+        assert A(F)(F) == F
